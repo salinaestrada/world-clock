dbe7a0dcfdfad7b685f7d166e0d126d0724866fc
salinaestrada <salina.lenay@gmail.com>
2024-04-12 23:15:25 -0500

    Los Angeles and Paris Time JS

--- index.js
+++
@@ -1,6 +0,0 @@
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesDateElement.querySelector(".time");
-
-losAngelesDateElement.innerHTML = "date";
-losAngelesTimeElement.innerHTML = "time <smal>am</small>";
