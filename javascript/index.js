3bb60c5c94564cd52cf0fb2171651994a2a8dbaf
salinaestrada <salina.lenay@gmail.com>
2024-04-13 00:33:44 -0500

    Added current time functionality

--- javascript/index.js
+++ javascript/index.js
@@ -29,6 +29,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess(true);
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
